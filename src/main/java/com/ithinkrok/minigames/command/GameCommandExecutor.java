b78ed0f25fa99760d0a80fec0ec84e7e92720707
paul <pauldurbz@gmail.com>
2016-01-25 19:30:53 +0000

    Removed some remains of the old command API

--- src/main/java/com/ithinkrok/minigames/command/GameCommandExecutor.java
+++
@@ -1,9 +0,0 @@
-package com.ithinkrok.minigames.command;
-
-/**
- * Created by paul on 12/01/16.
- */
-public interface GameCommandExecutor {
-
-    boolean onCommand(CommandSender sender, Command command);
-}
--- src/main/java/com/ithinkrok/minigames/command/GameCommandHandler.java
+++ src/main/java/com/ithinkrok/minigames/command/GameCommandHandler.java
@@ -1,18 +1,10 @@
 package com.ithinkrok.minigames.command;
 
-import com.ithinkrok.minigames.Game;
 import com.ithinkrok.minigames.GameGroup;
-import com.ithinkrok.minigames.Kit;
-import com.ithinkrok.minigames.User;
 import com.ithinkrok.minigames.lang.LanguageLookup;
-import com.ithinkrok.minigames.team.TeamIdentifier;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,14 +14,8 @@ import java.util.Map;
 /**
  * Created by paul on 12/01/16.
  */
-public class GameCommandHandler implements CommandExecutor {
+public class GameCommandHandler {
 
-    private final Game game;
-    private Map<String, GameCommandExecutor> executors = new HashMap<>();
-
-    public GameCommandHandler(Game game) {
-        this.game = game;
-    }
 
     public static List<String> splitStringIntoArguments(String command) {
         if (command.startsWith("/")) command = command.substring(1);
@@ -73,57 +59,6 @@ public class GameCommandHandler implements CommandExecutor {
 
         if (current.length() > 0) result.add(current.toString());
         return result;
-    }
-
-    public void addExecutor(GameCommandExecutor executor, String... commandNames) {
-        for (String commandName : commandNames) {
-            executors.put(commandName, executor);
-        }
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
-        if (!executors.containsKey(command.getName().toLowerCase())) return false;
-
-        List<String> correctedArgs = mergeArgumentsInQuotes(args);
-
-        Map<String, Object> arguments = parseArgumentListToMap(correctedArgs);
-
-        User user = null;
-        GameGroup gameGroup = game.getSpawnGameGroup();
-        TeamIdentifier teamIdentifier = null;
-
-        User userSender = null;
-        if (sender instanceof Player) {
-            user = userSender = game.getUser(((Player) sender).getUniqueId());
-        } else if (arguments.containsKey("u")) {
-            OfflinePlayer player = Bukkit.getPlayer(arguments.get("u").toString());
-            if (player != null) user = game.getUser(player.getUniqueId());
-        }
-
-        if (user != null) {
-            gameGroup = user.getGameGroup();
-            teamIdentifier = user.getTeamIdentifier();
-        }
-
-        if (gameGroup == null) gameGroup = game.getSpawnGameGroup();
-
-        if (arguments.containsKey("t")) {
-            teamIdentifier = gameGroup.getTeamIdentifier(arguments.get("t").toString());
-        }
-
-        Kit kit = null;
-        if (arguments.containsKey("k")) {
-            kit = gameGroup.getKit(arguments.get("k").toString());
-        }
-
-        Command gameCommand = new Command(command.getName(), arguments, gameGroup, user, teamIdentifier, kit);
-
-        com.ithinkrok.minigames.command.CommandSender messagable;
-        if (userSender != null) messagable = userSender;
-        else messagable = new ConsoleSender(gameGroup);
-
-        return executors.get(command.getName()).onCommand(messagable, gameCommand);
     }
 
     /**
