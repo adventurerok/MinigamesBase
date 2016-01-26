87099cf28f003dcb3c8d3928135877927a096d92
paul <pauldurbz@gmail.com>
2016-01-26 18:37:27 +0000

    The help command now shows commands from other plugins

--- src/main/java/com/ithinkrok/minigames/command/HelpCommand.java
+++ src/main/java/com/ithinkrok/minigames/command/HelpCommand.java
@@ -2,8 +2,14 @@ package com.ithinkrok.minigames.command;
 
 import com.ithinkrok.minigames.event.CommandEvent;
 import com.ithinkrok.minigames.event.MinigamesEventHandler;
+import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,33 +21,72 @@ public class HelpCommand implements Listener {
     public void onCommand(CommandEvent event) {
         if (!event.getCommand().requireGameGroup(event.getCommandSender())) return;
 
-
+        Command command = event.getCommand();
         CommandSender sender = event.getCommandSender();
 
         if (event.getCommand().hasArg(0)) {
-            String commandName = event.getCommand().getStringArg(0, "unspecified").toLowerCase();
-            CommandConfig commandConfig = event.getCommand().getGameGroup().getCommand(commandName);
-
-            if (commandConfig == null) {
-                sender.sendLocale("command.help.unknown", commandName);
-                return;
-            }
-
-            String usage = commandConfig.getUsage().replace("<command>", commandName);
-            String desc = commandConfig.getDescription();
-
-            sender.sendLocaleNoPrefix("command.help.usage", commandName, usage, desc);
+            onUsageCommand(sender, command);
         } else {
-            sender.sendLocaleNoPrefix("command.help.title");
-            //Commands are stored as a TreeMap so no need to sort
-            Map<String, CommandConfig> commands = event.getCommand().getGameGroup().getCommands();
-
-            for (CommandConfig command : commands.values()) {
-                sender.sendLocaleNoPrefix("command.help.line", command.getName(), command.getDescription());
-            }
+            onListCommand(sender, command);
         }
 
 
         //TODO Split into pages
+    }
+
+    private void onUsageCommand(CommandSender sender, Command command) {
+        String commandName = command.getStringArg(0, "unspecified").toLowerCase();
+        CommandConfig commandConfig = command.getGameGroup().getCommand(commandName);
+
+        String usage, desc;
+
+        if (commandConfig == null) {
+            PluginCommand pluginCommand = Bukkit.getPluginCommand(commandName);
+
+            if(pluginCommand == null) {
+                sender.sendLocale("command.help.unknown", commandName);
+                return;
+            }
+
+            usage = pluginCommand.getUsage();
+            desc = pluginCommand.getDescription();
+        } else {
+            usage = commandConfig.getUsage();
+            desc = commandConfig.getDescription();
+        }
+
+        usage = usage.replace("<command>", commandName);
+
+        sender.sendLocaleNoPrefix("command.help.usage", commandName, usage, desc);
+    }
+
+    private void onListCommand(CommandSender sender, Command command) {
+        sender.sendLocaleNoPrefix("command.help.title");
+        //Commands are stored as a TreeMap so no need to sort
+        Map<String, CommandConfig> commands = command.getGameGroup().getCommands();
+
+        List<String> outputLines = new ArrayList<>();
+
+        for (CommandConfig commandConfig : commands.values()) {
+            if (!sender.hasPermission(commandConfig.getPermission())) continue;
+
+            outputLines.add(command.getGameGroup()
+                    .getLocale("command.help.line", commandConfig.getName(), commandConfig.getDescription()));
+        }
+
+        for (String commandName : Bukkit.getCommandAliases().keySet()) {
+            PluginCommand pluginCommand = Bukkit.getPluginCommand(commandName);
+
+            if (pluginCommand.getPermission() != null && !sender.hasPermission(pluginCommand.getPermission())) continue;
+
+            outputLines.add(command.getGameGroup()
+                    .getLocale("command.help.line", pluginCommand.getName(), pluginCommand.getDescription()));
+        }
+
+        Collections.sort(outputLines);
+
+        for (String line : outputLines) {
+            sender.sendMessageNoPrefix(line);
+        }
     }
 }
