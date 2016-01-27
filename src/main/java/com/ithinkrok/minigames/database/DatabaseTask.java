0021a947aaaba5da0ec16b5cea997b45a47669c8
paul <pauldurbz@gmail.com>
2016-01-27 18:14:19 +0000

    Added the new persistence system to fix not being able to use other plugins database classes in your database

--- src/main/java/com/ithinkrok/minigames/database/DatabaseTask.java
+++ src/main/java/com/ithinkrok/minigames/database/DatabaseTask.java
@@ -1,10 +1,8 @@
 package com.ithinkrok.minigames.database;
 
-import com.avaje.ebean.EbeanServer;
-
 /**
  * Created by paul on 17/01/16.
  */
 public interface DatabaseTask {
-    void run(EbeanServer database);
+    void run(DatabaseAccessor accessor);
 }
--- src/main/java/com/ithinkrok/minigames/database/Persistence.java
+++ src/main/java/com/ithinkrok/minigames/database/Persistence.java
@@ -1,10 +1,16 @@
 package com.ithinkrok.minigames.database;
 
+import com.avaje.ebean.EbeanServer;
+import com.avaje.ebean.Query;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.persistence.OptimisticLockException;
 import javax.persistence.PersistenceException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
@@ -17,6 +23,8 @@ public class Persistence extends Thread {
     private boolean stop = false;
 
     private final ConcurrentLinkedQueue<DatabaseTask> threadTasks = new ConcurrentLinkedQueue<>();
+
+    private final DatabaseAccessor accessor = new PersistenceDatabaseAccessor();
 
     public Persistence(Plugin plugin) {
         this.plugin = plugin;
@@ -47,7 +55,7 @@ public class Persistence extends Thread {
 
             while((task = threadTasks.poll()) != null) {
                 try {
-                    task.run(plugin.getDatabase());
+                    task.run(accessor);
                 } catch (Exception e) {
                     System.out.println("Exception while doing DatabaseTask");
                     e.printStackTrace();
@@ -75,5 +83,45 @@ public class Persistence extends Thread {
 
     public void doTask(DatabaseTask task) {
         threadTasks.add(task);
+    }
+
+    private static class PersistenceDatabaseAccessor implements DatabaseAccessor {
+
+        private final Map<Class<?>, String> pluginNames = new HashMap<>();
+
+        @Override
+        public EbeanServer getDatabase(Class<?> databaseClass) {
+            if(pluginNames.containsKey(databaseClass)) {
+                return Bukkit.getPluginManager().getPlugin(pluginNames.get(databaseClass)).getDatabase();
+            }
+
+            for(Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
+                EbeanServer database = plugin.getDatabase();
+
+                try {
+                    database.find(databaseClass);
+
+                    pluginNames.put(databaseClass, plugin.getName());
+                    return database;
+                } catch (PersistenceException ignored) {}
+            }
+
+            throw new RuntimeException("No plugin has registered database class: " + databaseClass);
+        }
+
+        @Override
+        public <T> Query<T> find(Class<T> beanType) {
+            return getDatabase(beanType).find(beanType);
+        }
+
+        @Override
+        public <T> T createEntityBean(Class<T> type) {
+            return getDatabase(type).createEntityBean(type);
+        }
+
+        @Override
+        public void save(Object bean) throws OptimisticLockException {
+            getDatabase(bean.getClass()).save(bean);
+        }
     }
 }
