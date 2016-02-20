0ad998d602fd2970a6cf4ea763e0f2a7030e018e
paul <pauldurbz@gmail.com>
2016-02-20 13:32:53 +0000

    Support loading configs from both an internal and external config file

--- src/main/java/com/ithinkrok/minigames/api/SpecificPlugin.java
+++ src/main/java/com/ithinkrok/minigames/api/SpecificPlugin.java
@@ -7,6 +7,8 @@ import com.ithinkrok.util.config.MemoryConfig;
 import com.ithinkrok.util.config.YamlConfigIO;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+
 /**
  * Created by paul on 17/01/16.
  */
@@ -15,21 +17,49 @@ public class SpecificPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        boolean success = loadFromInternalConfig();
+        success |= loadFromExternalConfig();
+
+        if(!success) {
+            getLogger().warning("No gamegroup configs were loaded! This is odd.");
+        }
+    }
+
+    private boolean loadFromExternalConfig() {
+        Config config;
+
+        try {
+            config = YamlConfigIO.loadToConfig(getDataFolder().toPath().resolve("config.yml"), new MemoryConfig());
+        } catch (IOException ignored) {
+            return false;
+        }
+
+        return loadGameGroupConfigs(config);
+    }
+
+    private boolean loadFromInternalConfig() {
         Config config;
         try {
             config = YamlConfigIO.loadToConfig(getResource("gamegroups.yml"), new MemoryConfig());
-        } catch (InvalidConfigException e) {
-            System.out.println("Failed to load gamegroup configs config");
-            e.printStackTrace();
-            return;
+        } catch (InvalidConfigException ignored) {
+            return false;
         }
 
+        return loadGameGroupConfigs(config);
+    }
+
+    private boolean loadGameGroupConfigs(Config config) {
         Config gameGroupConfigs = config.getConfigOrEmpty("gamegroup_configs");
 
         Game game = BasePlugin.getGame();
 
+        boolean added = false;
+
         for(String name : gameGroupConfigs.getKeys(false)) {
             game.registerGameGroupConfig(name, gameGroupConfigs.getString(name));
+            added = true;
         }
+
+        return added;
     }
 }
