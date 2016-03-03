42ece3a2d2724ef417488a0b1f93e43cf07be789
paul <pauldurbz@gmail.com>
2016-03-03 20:14:16 +0000

    Send map credits to spectators when they join the map

--- src/main/java/com/ithinkrok/minigames/util/gamestate/SimpleInGameListener.java
+++ src/main/java/com/ithinkrok/minigames/util/gamestate/SimpleInGameListener.java
@@ -10,6 +10,8 @@ import com.ithinkrok.util.config.Config;
 import com.ithinkrok.util.event.CustomEventHandler;
 import com.ithinkrok.util.event.CustomListener;
 
+import java.util.List;
+
 /**
  * Created by paul on 31/01/16.
  */
@@ -21,6 +23,8 @@ public class SimpleInGameListener implements CustomListener {
 
     protected GameState gameState;
 
+    protected String mapCreditsLocale;
+
     @CustomEventHandler
     public void onListenerLoaded(ListenerLoadedEvent<GameGroup, GameState> event) {
         gameState = event.getRepresenting();
@@ -28,6 +32,7 @@ public class SimpleInGameListener implements CustomListener {
         Config config = event.getConfigOrEmpty();
 
         spectatorItems = new ItemGiver(config.getConfigOrNull("spectator_items"));
+        mapCreditsLocale = config.getString("map_credits_locale", "start.map_credits");
         spectatorJoinLocaleStub = config.getString("spectator_join_locale_stub", "spectator.join");
     }
 
@@ -38,6 +43,16 @@ public class SimpleInGameListener implements CustomListener {
         event.getUser().teleport(event.getUserGameGroup().getCurrentMap().getSpawn());
 
         makeUserSpectator(event.getUser());
+
+        List<String> credits = event.getUserGameGroup().getCurrentMap().getInfo().getCredit();
+
+        if(!credits.isEmpty()) {
+            event.getUser().sendLocale(mapCreditsLocale);
+
+            for(String credit : credits) {
+                event.getUser().sendMessage(credit);
+            }
+        }
 
         for (int counter = 0; ; ++counter) {
             String message = event.getUserGameGroup().getLocale(spectatorJoinLocaleStub + "." + counter);
