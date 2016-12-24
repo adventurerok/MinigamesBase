6762c77beb98a4cc0171efed681cc53dd175d0a3
paul <pauldurbz@gmail.com>
2016-12-24 20:52:24 +0000

    Add MapEntityDeathEvents

---
+++ src/main/java/com/ithinkrok/minigames/api/event/map/MapEntityDeathEvent.java
@@ -0,0 +1,38 @@
+package com.ithinkrok.minigames.api.event.map;
+
+import com.ithinkrok.minigames.api.GameGroup;
+import com.ithinkrok.minigames.api.map.GameMap;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+/**
+ * Created by paul on 24/12/16.
+ */
+public class MapEntityDeathEvent extends MapEvent {
+
+    private final EntityDeathEvent event;
+
+    public MapEntityDeathEvent(GameGroup gameGroup, GameMap map, EntityDeathEvent event) {
+        super(gameGroup, map);
+        this.event = event;
+    }
+
+    public int getDroppedExp() {
+        return event.getDroppedExp();
+    }
+
+    public List<ItemStack> getDrops() {
+        return event.getDrops();
+    }
+
+    public LivingEntity getEntity() {
+        return event.getEntity();
+    }
+
+    public void setDroppedExp(int exp) {
+        event.setDroppedExp(exp);
+    }
+}
--- src/main/java/com/ithinkrok/minigames/base/bukkitlistener/GameBukkitListener.java
+++ src/main/java/com/ithinkrok/minigames/base/bukkitlistener/GameBukkitListener.java
@@ -233,6 +233,22 @@ public class GameBukkitListener implements Listener {
     }
 
     @EventHandler
+    public void eventEntityDeath(EntityDeathEvent event) {
+        String mapName = event.getEntity().getWorld().getName();
+        GameGroup gameGroup = game.getGameGroupFromMapName(mapName);
+        if (gameGroup == null) return;
+
+        //Don't send entity death events for users
+        if(EntityUtils.getActualUser(gameGroup, event.getEntity()) != null) return;
+
+        GameMap map = gameGroup.getCurrentMap();
+        if (!map.getWorld().getName().equals(mapName))
+            throw new RuntimeException("Map still registered to old GameGroup");
+
+        gameGroup.gameEvent(new MapEntityDeathEvent(gameGroup, map, event));
+    }
+
+    @EventHandler
     public void eventBlockIgnite(BlockIgniteEvent event) {
         String mapName = event.getBlock().getWorld().getName();
         GameGroup gameGroup = game.getGameGroupFromMapName(mapName);
