e70c07d17f8aedf90638aea7d6300b993a0e365c
paul <pauldurbz@gmail.com>
2018-04-13 18:36:23 +0100

    Create warp inventory

---
+++ src/main/java/com/ithinkrok/minigames/util/inventory/WarpInventory.java
@@ -0,0 +1,50 @@
+package com.ithinkrok.minigames.util.inventory;
+
+import com.ithinkrok.minigames.api.inventory.ClickableInventory;
+import com.ithinkrok.minigames.api.inventory.ClickableItem;
+import com.ithinkrok.minigames.api.inventory.event.UserClickItemEvent;
+import com.ithinkrok.minigames.api.map.MapPoint;
+import com.ithinkrok.minigames.api.user.User;
+import com.ithinkrok.minigames.api.util.InventoryUtils;
+import com.ithinkrok.minigames.api.util.MinigamesConfigs;
+import com.ithinkrok.util.config.Config;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+public class WarpInventory {
+
+
+    public static void showToUser(String title, List<Config> warps, User user) {
+        ClickableInventory inventory = new ClickableInventory(title);
+
+        for (Config warp : warps) {
+            ItemStack display;
+            display = MinigamesConfigs.getItemStack(warp, "item");
+
+            if(display == null) {
+                display = new ItemStack(Material.EMPTY_MAP);
+            }
+
+
+            display = InventoryUtils.setItemName(display, warp.getString("name"));
+            if(warp.contains("desc")) {
+                display = InventoryUtils.addLore(display, warp.getStringList("lore"));
+            }
+
+            ClickableItem clickable = new ClickableItem(display, warp.getInt("slot", -1)) {
+                @Override
+                public void onClick(UserClickItemEvent event) {
+                    MapPoint pos = MinigamesConfigs.getMapPoint(warp, "pos");
+                    event.getUser().teleport(pos);
+                }
+            };
+
+            inventory.addItem(clickable);
+        }
+
+        user.showInventory(inventory, null);
+    }
+
+}
